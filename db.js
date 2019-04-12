20c8a49f5a3f2925eacad47f750ab3d8e56183b1
Choco0914 <heojeongho1992@gmail.com>
2019-04-12 21:42:40 +0900

    change upload, changePassword, editVideo in HTML and Home Controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 24231,
+    title: "Video Awesome",
+    description: "This is something I love",
+    views: 200,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 1111,
+      name: "Jeongho",
+      email: "jeongho@gmail.com"
+    }
+  },
+  {
+    id: 4544,
+    title: "Video Greate",
+    description: "This is something I love",
+    views: 200,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 1111,
+      name: "Jeongho",
+      email: "jeongho@gmail.com"
+    }
+  },
+  {
+    id: 3556,
+    title: "Video Wonderful",
+    description: "This is something I love",
+    views: 200,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 1111,
+      name: "Jeongho",
+      email: "jeongho@gmail.com"
+    }
+  }
+];
