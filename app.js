6ea1109287fc6b9bf309fc38c9ed18d8849b92d8
Choco0914 <heojeongho1992@gmail.com>
2019-04-13 18:31:51 +0900

    using body-parser for post data

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 app.set("view engine", "pug");
 app.use(helmet());
 app.use(cookieParser());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
