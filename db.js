f710f6f43a30b412333cbd52922471ff4b92e117
Choco0914 <heojeongho1992@gmail.com>
2019-04-15 09:52:14 +0900

    installed mongoDB, Mongoose and conected mongodb Configuraing Dot Env

--- db.js
+++ db.js
@@ -1,38 +1,16 @@
-export const videos = [
-  {
-    id: 24231,
-    title: "Video Awesome",
-    description: "This is something I love",
-    views: 200,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 1111,
-      name: "Jeongho",
-      email: "jeongho@gmail.com"
-    }
-  },
-  {
-    id: 4544,
-    title: "Video Greate",
-    description: "This is something I love",
-    views: 200,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 1111,
-      name: "Jeongho",
-      email: "jeongho@gmail.com"
-    }
-  },
-  {
-    id: 3556,
-    title: "Video Wonderful",
-    description: "This is something I love",
-    views: 200,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 1111,
-      name: "Jeongho",
-      email: "jeongho@gmail.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
