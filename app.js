113d25df1dc056dcf56de0f413a1ef626df1902e
Choco0914 <heojeongho1992@gmail.com>
2019-04-16 10:12:46 +0900

    Uploading and Creating a video part two

--- app.js
+++ app.js
@@ -21,5 +21,6 @@ app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use("/uploads", express.static("uploads"));
 
 export default app;
