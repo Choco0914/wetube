d3919e13e151500fd631066185a6e1c697f91e2d
Choco0914 <heojeongho1992@gmail.com>
2019-04-18 17:19:23 +0900

    finish Styling

--- middlewares.js
+++ middlewares.js
@@ -8,7 +8,7 @@ export const localsMiddleware = (req, res, next) => {
   app.locals.siteName = "Wetube";
   app.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: false,
+    isAuthenticated: true,
     id: 1
   };
   next();
