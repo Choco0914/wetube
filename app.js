576ca37a402ceb25152399f459492f4909d21f0b
Choco0914 <heojeongho1992@gmail.com>
2019-04-19 14:51:24 +0900

    Use MongoStore and Middlewares

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
+import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
-import { localsMiddleware } from "./middlewares";
 
 import "./passport";
 
 const app = express();
 
-app.set("view engine", "pug");
+const CokieStore = MongoStore(session);
+
+dotenv.config();
+
 app.use(helmet());
-app.use("/static", express.static("static"));
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(localsMiddleware);
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- init.js
+++ init.js
@@ -1,14 +1,10 @@
-import dotenv from "dotenv";
 import app from "./app";
 import "./db";
 import "./models/Comment";
 import "./models/Video";
 import "./models/User";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
-
 const handleListening = () => {
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 };
