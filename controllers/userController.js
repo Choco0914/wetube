1b327f5118d7075df03bba57cc4f25f9bd9ae866
Choco0914 <heojeongho1992@gmail.com>
2019-04-21 11:37:26 +0900

    add change password funciton

--- controllers/userController.js
+++ controllers/userController.js
@@ -141,9 +141,25 @@ export const postEditProfile = async (req, res) => {
     });
     res.redirect(routes.me);
   } catch (error) {
-    res.render("editProfile", { pageTitle: "Edit Profile" });
+    res.redirect(routes.editProfile);
   }
 };
 
-export const changePassword = (req, res) =>
+export const getChangePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
+
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword1 }
+  } = req;
+  try {
+    if (newPassword !== newPassword1) {
+      res.redirect(`${routes.users}${routes.changePassword}`);
+    } else {
+      await req.user.changePassword(oldPassword, newPassword);
+      res.redirect(routes.me);
+    }
+  } catch (error) {
+    res.redirect(`${routes.users}${routes.changePassword}`);
+  }
+};
