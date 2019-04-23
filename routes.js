26126ed07a8f8b97d3bfce3c98b07ae327b96fd3
Choco0914 <heojeongho1992@gmail.com>
2019-04-23 20:40:29 +0900

    API Adding a Comment part Two

--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const GOOGLE_CALLBACK = "/auth/google/callback";
 // API
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -76,7 +77,8 @@ const routes = {
   google: GOOGLE,
   googleCallback: GOOGLE_CALLBACK,
   api: API,
-  registerView: REGISTER_VIEW
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
